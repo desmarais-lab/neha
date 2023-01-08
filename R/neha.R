09b567cad03b3c1e51ca4475b22a2528d9702061
Fridolin Linder <fridolin.linder@gmail.com>
2023-01-08 22:34:53 +0100

    Reformat portions of the `neha` example code to avoid R CMD CHECK NOTES about lines longer than 100 characters

--- R/neha.R
+++ R/neha.R
@@ -236,7 +236,9 @@ neha_geta <- function(eha_data, node, time, event, cascade, ncore=2){
 #' node <- paste("n",as.character(rep(1:nodes,times)),sep="")
 #' intercept <- rep(1,length(time))
 #' covariate <- runif(length(time))-2
-#' data_for_sim <- data.frame(time,node,intercept,covariate,stringsAsFactors=FALSE)
+#' data_for_sim <- data.frame(
+#'   time, node, intercept, covariate, stringsAsFactors = FALSE
+#' )
 #'
 #' # regression parameters
 #' beta <- cbind(c(-2.5,.25))
@@ -244,7 +246,11 @@ neha_geta <- function(eha_data, node, time, event, cascade, ncore=2){
 #'
 #' # generate network effects
 #' possible_ties <- rbind(t(combn(1:nodes,2)),t(combn(1:nodes,2))[,c(2,1)])
-#' possible_ties <- paste(paste("n",possible_ties[,1],sep=""),paste("n",possible_ties[,2],sep=""),sep="_")
+#' possible_ties <- paste(
+#'   paste("n",possible_ties[,1],sep=""),
+#'   paste("n",possible_ties[,2],sep=""),
+#'   sep="_"
+#' )
 #' ties <- sample(possible_ties,nties)
 #' gamma <- cbind(rep(1.5,length(ties)))
 #' rownames(gamma) <- ties
@@ -253,17 +259,40 @@ neha_geta <- function(eha_data, node, time, event, cascade, ncore=2){
 #' simulated_data <- NULL
 #'
 #' # generate the data one cascade at a time
-#' for(c in 1:cascades){
-#'   simulated_cascade <- simulate_neha_discrete(x=data_for_sim,node="node",time="time",beta=beta,gamma=gamma,a=-6)
-#'  simulated_cascade <- data.frame(simulated_cascade,cascade=c,stringsAsFactors=F)
-#'   simulated_data <- rbind(simulated_data,simulated_cascade)
-#' }
+#'for(c in 1:cascades) {
+#'  simulated_cascade <-
+#'    simulate_neha_discrete(
+#'      x = data_for_sim,
+#'      node = "node",
+#'      time = "time",
+#'      beta = beta,
+#'      gamma = gamma,
+#'      a = -6
+#'    )
+#'  simulated_cascade <-
+#'    data.frame(simulated_cascade,
+#'               cascade = c,
+#'               stringsAsFactors = F)
+#'  simulated_data <- rbind(simulated_data, simulated_cascade)
+#'}
 #'
-#' # infer edges
-#' neha_results <- neha(simulated_data,node="node",time="time",event="event",cascade="cascade",covariates="covariate",ncore=3)
+#'# infer edges
+#'neha_results <-
+#'  neha(
+#'    simulated_data,
+#'    node = "node",
+#'    time = "time",
+#'    event = "event",
+#'    cascade = "cascade",
+#'    covariates = "covariate",
+#'    ncore = 3
+#'  )
 #'
 #' # estimate NEHA logistic regression
-#' neha_estimate <- glm(neha_results$combined_formula,data=neha_results$data_for_neha,family=binomial)
+#' neha_estimate <-
+#'   glm(neha_results$combined_formula,
+#'       data = neha_results$data_for_neha,
+#'       family = binomial)
 #' summary(neha_estimate)
 #' }
 #'
